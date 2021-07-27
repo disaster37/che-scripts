ad06d805e980b6f6e0752f733095b739d83491fd
disaster37 <linuxworkgroup@hotmail.com>
2021-07-27 15:11:19 +0000

    Add ubi script
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

---
+++ ubi.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DOCKER_GID=1724
+DOCKER_UID=1724
+GROUP=theia
+USER=theia
+APP_HOME=/home/theia
+
+# Install extra package
+microdnf install -y  sudo git wget curl ca-certificates zip vim make bash &&\
+microdnf clean all && \
+rm -rf /tmp/* /var/tmp/*
+
+# Create user and group
+groupadd --gid ${DOCKER_GID} ${GROUP} && \
+useradd -c "User for che" -d ${APP_HOME} -m -g ${GROUP} -s /bin/bash -u ${DOCKER_UID} ${USER} &&\
+echo "%${GROUP} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${GROUP}
+
